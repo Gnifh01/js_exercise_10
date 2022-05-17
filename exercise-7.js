4f7ee2d0d9db582e4e235a91319eacc13b299f95
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-05-17 18:11:23 +0200

    first commit

---
+++ exercise-7.js
@@ -0,0 +1,25 @@
+console.log("FUNCTION - #1");
+function sumUntil(maxValue) {
+  let somma = 0,
+    value = maxValue + 1;
+  for (i = 1; i < value; i++) {
+    somma += i;
+  }
+  return `La somma totale dei numeri da 1 a ${maxValue} è: ${somma}!`;
+}
+
+console.log(sumUntil(5));
+
+console.log("-----------------------------------------");
+
+console.log("FUNCTION - #2");
+function sumUntilVersionTwo(maxValue) {
+  let arrNumeri = [];
+  for (let i = 1; i < maxValue + 1; i++) {
+    arrNumeri.push(i);
+  }
+  let sommaTot = arrNumeri.reduce((sum, current) => sum + current, 0);
+  return `La somma totale dei numeri da 1 a ${maxValue} è: ${sommaTot}!`;
+}
+
+console.log(sumUntilVersionTwo(5));
